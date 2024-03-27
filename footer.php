179fcca290c7e776246703ddb562244fca750295
Nguyễn Uy <120319180+NguyenUy02@users.noreply.github.com>
2024-03-27 23:52:03 +0700

    Giao dien nguoi dung

---
+++ footer.php
@@ -0,0 +1,110 @@
+<!-- Footer Start -->
+        <div class="container-fluid bg-dark text-white-50 footer pt-5 mt-5">
+            <div class="container py-5">
+                <div class="pb-4 mb-4" style="border-bottom: 1px solid rgba(100, 175, 24, 0.5) ;">
+                    <div class="row g-4">
+                        <!-- <div class="col-lg-3">
+                            <a href="#">
+                                <h1 class="text-primary mb-0">Nguyễn Uy</h1>
+                                <p class="text-secondary mb-0">Sản phẩm</p>
+                            </a>
+                        </div> -->
+                        <div class="col-lg-6">
+                            <div class="position-relative mx-auto">
+                                <!-- <input class="form-control border-0 w-100 py-3 px-4 rounded-pill" type="number" placeholder="Your Email">
+                                <button type="submit" class="btn btn-primary border-0 border-secondary py-3 px-4 position-absolute rounded-pill text-white" style="top: 0; right: 0;">Gửi email</button> -->
+                            </div>
+                        </div>
+                        <div class="col-lg-3">
+                            <div class="d-flex justify-content-end pt-3">
+                                <a class="btn  btn-outline-secondary me-2 btn-md-square rounded-circle" href=""><i class="fab fa-twitter"></i></a>
+                                <a class="btn btn-outline-secondary me-2 btn-md-square rounded-circle" href=""><i class="fab fa-facebook-f"></i></a>
+                                <a class="btn btn-outline-secondary me-2 btn-md-square rounded-circle" href=""><i class="fab fa-youtube"></i></a>
+                                <a class="btn btn-outline-secondary btn-md-square rounded-circle" href=""><i class="fab fa-linkedin-in"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row g-5">
+                    <div class="col-lg-3 col-md-6">
+                        <div class="footer-item">
+                            <!-- <h4 class="text-light mb-3"></h4> -->
+                            <!-- <p class="mb-4"></p> -->
+                            <h4 class="text-light mb-3">Liên hệ</h4>
+                            <!-- <a href="" class="btn border-secondary py-2 px-4 rounded-pill text-primary">Read More</a> -->
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="d-flex flex-column text-start footer-item">
+                            <h4 class="text-light mb-3"></h4>
+                            <p>Address: Nha trang</p>
+                            <!-- <a class="btn-link" href=""></a>
+                            <a class="btn-link" href=""></a>
+                            <a class="btn-link" href=""></a>
+                            <a class="btn-link" href=""></a>
+                            <a class="btn-link" href=""></a> -->
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="d-flex flex-column text-start footer-item">
+                            <h4 class="text-light mb-3"></h4>
+                            <p>Email: uy.n.cntt@ntu.edu.vn</p>
+                            <!-- <a class="btn-link" href=""></a>
+                            <a class="btn-link" href=""></a>
+                            <a class="btn-link" href=""></a>
+                            <a class="btn-link" href=""></a>
+                            <a class="btn-link" href=""></a> -->
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="footer-item">
+                            <h4 class="text-light mb-3"></h4>
+                            <!--
+                            <p>Address: Nha trang</p>
+                            <p>Email: uy.n.cnttÂntu.edu.vn</p> -->
+                            <p>Phone: 0346729653</p>                           
+                            <!-- <img src="img/payment.png" class="img-fluid" alt=""> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Footer End -->
+
+        <!-- Copyright Start -->
+        <div class="container-fluid copyright bg-dark py-4">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                        <span class="text-light"><a href="#"><i class="fas fa-copyright text-light me-2"></i>Nguyễn Uy</a> - 62CNTT1</span>
+                    </div>
+                    <div class="col-md-6 my-auto text-center text-md-end text-white">
+                        <!--/*** This template is free as long as you keep the below author’s credit link/attribution link/backlink. ***/-->
+                        <!--/*** If you'd like to use the template without the below author’s credit link/attribution link/backlink, ***/-->
+                        <!--/*** you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". ***/-->
+                         <a class="border-bottom" href=""></a> <a class="border-bottom" href=""></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Copyright End -->
+
+
+
+        <!-- Back to Top -->
+        <a href="#" class="btn btn-primary border-3 border-primary rounded-circle back-to-top"><i class="fa fa-arrow-up"></i></a>   
+
+        
+    <!-- JavaScript Libraries -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script> -->
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/lightbox/js/lightbox.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+    </body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,200 @@
+<?php include 'header.php' ?>
+<title>Trang chủ</title>
+    
+    <!-- Modal Search Start -->
+    <div class="modal fade" id="searchModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-fullscreen">
+            <div class="modal-content rounded-0">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Tìm kiếm</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body d-flex align-items-center">
+                    <div class="input-group w-75 mx-auto d-flex">
+                        <input type="search" class="form-control p-3" placeholder="keywords" aria-describedby="search-icon-1">
+                        <span id="search-icon-1" class="input-group-text p-3"><i class="fa fa-search"></i></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Modal Search End -->
+
+
+    <!-- Poster Start -->
+    <div class="container-fluid py-5 hero-header">
+        <div class="container">
+            <div class="row g-5 align-items-center">
+                <div class="col-md-12 col-lg-7">
+                    <h4 class="mb-3 text-secondary">MiniMart</h4>
+                    <h1 class="mb-5 display-3 text-primary">Drink and Food</h1>
+                    <div class="position-relative mx-auto">
+                        <input class="form-control border-2 border-secondary w-75 py-3 px-4 rounded-pill" type="number" placeholder="Tìm kiếm">
+                        <button type="submit" class="btn btn-primary border-2 border-secondary py-3 px-4 position-absolute rounded-pill text-white h-100" style="top: 0; right: 25%;">Tìm kiếm</button>
+                    </div>
+                </div>
+                <!-- Banner -->
+                <div class="col-md-12 col-lg-5">
+                    <div id="carouselId" class="carousel slide position-relative" data-bs-ride="carousel">
+                        <div class="carousel-inner" role="listbox">
+                            <div class="carousel-item active rounded">
+                                <img src="img/hero-img-1.png" class="img-fluid w-100 h-100 bg-secondary rounded" alt="First slide">
+                                <!-- <a href="#" class="btn px-4 py-2 text-white rounded"> </a> -->
+                            </div>
+                            <div class="carousel-item rounded">
+                                <img src="img/hero-img-2.jpg" class="img-fluid w-100 h-100 rounded" alt="Second slide">
+                                <!-- <a href="#" class="btn px-4 py-2 text-white rounded"></a> -->
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselId" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselId" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Poster End -->
+
+    <!-- Vesitable Shop Start-->
+    <div class="container-fluid vesitable py-5">
+    <div class="container py-5">
+    <h1 class="mb-0">Sản phẩm nổi bật</h1>
+    <div class="owl-carousel vegetable-carousel justify-content-center">
+        <?php
+        $result = mysqli_query($conn, "SELECT * FROM sanpham  LIMIT 12");
+        if (mysqli_num_rows($result) <> 0) {
+            while ($rows = mysqli_fetch_assoc($result)) {
+        ?>
+        <div class="border border-primary rounded position-relative vesitable-item" style="height: 400px;">
+            <div class="vesitable-img">
+                <img src="img/<?php echo $rows['ANH']; ?>" class="img-fluid rounded-top" style="height: 200px; object-fit: contain;" alt="">
+            </div>
+            <!-- <div class="text-white bg-primary px-3 py-1 rounded position-absolute" style="top: 10px; right: 10px;"><?php echo $rows['MALSP']; ?></div> -->
+            <div class="p-4 rounded-bottom">
+                <h5><?php echo $rows['TENSP']; ?></h5>
+                <p><?php echo $rows['GIA']; ?></p>
+                <div class="d-flex justify-content-between flex-wrap">
+                    <p class="text-dark  fw-bold "><?php echo $rows['SALE']; ?></p>
+                    
+                </div>
+                <div class="text-center"> <!-- Thêm lớp text-center để căn giữa nút -->
+                    <a href="#" class="btn border border-secondary rounded-pill text-primary"><i class="fa fa-shopping-bag me-2 text-primary"></i> Thêm vào giỏ</a>
+                </div>
+            </div>
+        </div>
+        <?php
+            }
+        }
+        ?>
+    </div>
+</div>
+    </div>
+    <!-- Vesitable Shop End -->
+
+    <!-- Bestsaler Product Start -->
+    <div class="container-fluid">
+        <div class="container">
+            <div class="text-center mx-auto mb-5" style="max-width: 700px;">
+                <h1 class="display-4">Sản phẩm giảm giá</h1>
+                <p>Chương trình giảm giá hấp dẫn</p>
+            </div>
+            <div class="row g-4">
+            <?php
+            $result = mysqli_query($conn, "SELECT * FROM sanpham WHERE SALE > 0");
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $tenSP = $row['TENSP'];
+                    $anh = $row['ANH'];
+                    $gia = $row['GIA'];
+                    $sale = $row['SALE'];
+                    
+            ?>
+            <div class="col-lg-6 col-xl-4">
+                <div class="p-4 rounded bg-light d-flex flex-fill">
+                    <div class="row align-items-center">
+                        <div class="col-6">
+                            <img src="img/<?php echo $anh; ?>" class="img-fluid rounded-circle w-100" style="width: 150px; height: 150px;" alt="">
+                        </div>
+                        <div class="col-6">
+                            <a href="#" class="h5"><?php echo $tenSP; ?></a>
+                            <h4 class="mb-3">
+                                <del><?php echo number_format($gia, 0, ".", ","); ?></del>
+                                <span class="text-danger"><?php echo number_format($sale, 0, ".", ","); ?></span>
+                            </h4>
+                            <a href="#" class="btn btn-sm border border-secondary rounded-pill px-3 text-primary"><i class="fa fa-shopping-bag me-2 text-primary"></i> Thêm vào giỏ hàng</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                <?php
+                    }
+                } else {
+                    echo "Không có sản phẩm giảm giá.";
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+    <!-- Bestsaler Product End -->
+
+    <!-- Featurs Section Start -->
+    <div class="container-fluid featurs py-5">
+        <div class="container py-5">
+            <div class="row g-4">
+                <div class="col-md-6 col-lg-3">
+                    <div class="featurs-item text-center rounded bg-light p-4">
+                        <div class="featurs-icon btn-square rounded-circle bg-secondary mb-5 mx-auto">
+                            <i class="fas fa-car-side fa-3x text-white"></i>
+                        </div>
+                        <div class="featurs-content text-center">
+                            <h5>Giao hàng miễn phí</h5>
+                            <p class="mb-0">Áp dụng cho hóa đơn trên 300k</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="featurs-item text-center rounded bg-light p-4">
+                        <div class="featurs-icon btn-square rounded-circle bg-secondary mb-5 mx-auto">
+                            <i class="fas fa-user-shield fa-3x text-white"></i>
+                        </div>
+                        <div class="featurs-content text-center">
+                            <h5>Thanh toán dễ dàng</h5>
+                            <p class="mb-0">Nhiều hình thức thanh toán</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="featurs-item text-center rounded bg-light p-4">
+                        <div class="featurs-icon btn-square rounded-circle bg-secondary mb-5 mx-auto">
+                            <i class="fas fa-exchange-alt fa-3x text-white"></i>
+                        </div>
+                        <div class="featurs-content text-center">
+                            <h5>Không đổi trả</h5>
+                            <p class="mb-0">Mua rồi không trả</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="featurs-item text-center rounded bg-light p-4">
+                        <div class="featurs-icon btn-square rounded-circle bg-secondary mb-5 mx-auto">
+                            <i class="fa fa-phone-alt fa-3x text-white"></i>
+                        </div>
+                        <div class="featurs-content text-center">
+                            <h5>Tư vấn 24/7</h5>
+                            <p class="mb-0">Liên hệ</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Featurs Section End -->
+
+    <!-- Footer Start -->
+    <?php include 'footer.php' ?>
